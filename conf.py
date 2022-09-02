4557dfed6d75b314defa3a6bd45df95395ee1dd6
jpstaub <jpstaub1@gmail.com>
2022-09-01 22:22:21 -0400

    reformat

--- conf.py
+++ conf.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep  1 20:32:19 2022
-
-@author: admin
-
-https://docs.pyvista.org/user-guide/jupyter/pythreejs.html
-"""
-
 extensions = [
     "jupyter_sphinx"
 ]
